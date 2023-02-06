23ecfa5785c3608a08b483eb74e1f3948c600838
AxelMaksel <am3mail@gmail.comC:/Python/Python39/python.exe>
2023-02-06 21:00:58 +0700

    Maria zamena

--- keyboards/inline/choice_buttons.py
+++ keyboards/inline/choice_buttons.py
@@ -2,26 +2,29 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 # from handlers.users.game import board
 
 
-choice = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text="=", callback_data="move:0"),
-            InlineKeyboardButton(text="=", callback_data="move:1"),
-            InlineKeyboardButton(text="=", callback_data="move:2"),
+def move(board):
+    choice = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text=board[0], callback_data="move:0"),
+                InlineKeyboardButton(text=board[1], callback_data="move:1"),
+                InlineKeyboardButton(text=board[2], callback_data="move:2"),
 
-        ],
-        [
-            InlineKeyboardButton(text="=", callback_data="move:3"),
-            InlineKeyboardButton(text="=", callback_data="move:4"),
-            InlineKeyboardButton(text="=", callback_data="move:5"),
+            ],
+            [
+                InlineKeyboardButton(text=board[3], callback_data="move:3"),
+                InlineKeyboardButton(text=board[4], callback_data="move:4"),
+                InlineKeyboardButton(text=board[5], callback_data="move:5"),
 
-        ],
-        [
-            InlineKeyboardButton(text="=", callback_data="move:6"),
-            InlineKeyboardButton(text="=", callback_data="move:7"),
-            InlineKeyboardButton(text="=", callback_data="move:8"),
+            ],
+            [
+                InlineKeyboardButton(text=board[6], callback_data="move:6"),
+                InlineKeyboardButton(text=board[7], callback_data="move:7"),
+                InlineKeyboardButton(text=board[8], callback_data="move:8"),
 
-        ],
+            ],
 
-    ]
-)
+        ]
+    )
+
+    return choice
